6f3dbd811b8fc913023f605899cdc9ab7810f638
samzee1545 <odayelabolasam@gmail.com>
2023-02-19 15:37:01 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
-include <stdio.h>
-
+#include <stdio.h>
+/**
+ * main - function to print to the standard output with puts function
+ * Return: 0 successful
+ */
 int main(void)
 {
-	puts("Programmming is like building a multilingual puzzle\n");
+	puts("/"Programmming is like building a multilingual puzzle");
+	return (0);
 }
