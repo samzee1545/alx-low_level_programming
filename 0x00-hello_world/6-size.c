48674bbc98a3074fdab65795a8b5596f54c95552
samzee1545 <odayelabolasam@gmail.com>
2023-02-19 17:40:47 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
 
 
