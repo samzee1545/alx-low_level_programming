9487069e445782f661d5333ce921677fa03f7fbe
samzee1545 <odayelabolasam@gmail.com>
2023-02-19 16:59:30 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - uses printf to print "with proper grammar,
+ *                              but the outcome is a piece of art,"
+ *
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
