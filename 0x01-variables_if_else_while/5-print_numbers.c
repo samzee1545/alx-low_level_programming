5dc7b3bb63ce6a0aa275b9931d3c55d412dbd9b1
samzee1545 <odayelabolasam@gmail.com>
2023-02-20 18:16:48 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing single base 10 numbers
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
