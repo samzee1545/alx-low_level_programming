2af354b38cfa38c2bcc3c426fe4dbfc5d450a26c
samzee1545 <odayelabolasam@gmail.com>
2023-02-20 18:02:46 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: printing alphabets except some letters
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 26)
+	{
+		if (i != 4 && i != 16)
+		{
+			putchar(97 + i);
+			i++;
+		}
+		else
+		{
+			putchar(97 + i + 2);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
