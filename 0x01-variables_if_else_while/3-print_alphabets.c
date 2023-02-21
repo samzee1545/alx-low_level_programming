913ccf519f91dff2741e353154398982d68d66c0
samzee1545 <odayelabolasam@gmail.com>
2023-02-20 17:34:30 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing alphabets in both lowercase and upper
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while(i < 52)
+	{
+		if (i < 26)
+		{
+			putchar(97 + i);
+			i++;
+		}
+		else
+		{
+			putchar(39 + i);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
