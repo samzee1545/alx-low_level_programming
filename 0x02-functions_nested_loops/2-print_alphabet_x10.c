b88da5bd6c079dd2c19eb8f0fd4658be47146aba
samzee1545 <odayelabolasam@gmail.com>
2023-02-22 15:10:53 -0800

    print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,10 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char letters;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		for (letters = 'a'; letters < 'z'; i++)
+		for (letters = 'a'; letters <= 'z'; letters++)
 		{
 			_putchar(letters);
 		}
