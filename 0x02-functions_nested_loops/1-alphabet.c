e6fd43b71d0529aca96a48738fa6111ca75485b4
samzee1545 <odayelabolasam@gmail.com>
2023-02-21 18:06:55 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,19 @@
  * Description: printing lower case alphabets
  * Return: (0) always
  */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	while (i < 26)
+	{
+		putchar(97 + i);
+		i++;
+	}
+	putchar('\n');
+	return;
+}
+
 int main(void)
 {
 	print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#include <stdio.h>
-void print_alphabet(void)
-{
-	int i = 0;
-
-	while (i < 26)
-	{
-		putchar(97 + i);
-		i++;
-	}
-	putchar('\n');
-	return;
-}
+void print_alphabet(void);
